597f2b171403239eb369ed713922c9a09de2de59
YasmineHannouf <yasmina.hannouf.2018@gmail.com>
2022-12-07 15:39:56 +0200

    Completed step8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,14 @@
+const age= parseInt(prompt("enter your age"));
+let ans;
+if (age>18) {
+    ans="you are over 18";
+
+}
+
+else {
+    ans="you are under 18";
+
+}
+
+console.log(ans);
+alert(ans);
