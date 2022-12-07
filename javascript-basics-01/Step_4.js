cd072a73bfd21d7c0b0ba409503b07b83f8f58f1
YasmineHannouf <yasmina.hannouf.2018@gmail.com>
2022-12-07 11:59:25 +0200

    Completed step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name = prompt("Please enter your name");
+let surname = prompt("Please enter your surname");
+let city = prompt("Please enter your city");
+
+alert(name +"\n" + surname+"\n" + city);
